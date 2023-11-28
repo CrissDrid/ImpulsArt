e7919d25ae27f2dd46fcab6a9b8cee8f817594dd
shadowsouls444 <angel.acuna.meza@gmail.com>
2023-11-27 21:16:07 -0500

    Boton cancelar arreglado

--- ImpulsArt/PHP/EditarUsuario.php
+++ ImpulsArt/PHP/EditarUsuario.php
@@ -110,6 +110,11 @@ include_once "ConexionBD.php";
 </form>
     </div>
     
+    <script>
+    function RedirigirUsuario() {
+      window.location.href = "GestionUsuario.php";
+    }
+  </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="JS/RedirigirPagina.js"></script>
 </body>
